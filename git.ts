74fc7e2a86b072320a9cb0f9e0aa6c01dd008e8b
Mykyta Kuts <nikandroid2013@gmail.com>
2023-05-19 13:56:18 +0200

    feat: add git.ts

---
+++ git.ts
@@ -0,0 +1,5 @@
+// * 1. You added `OpenSettings` button to `UserScreen`
+
+// * 2. You refactored `useUser` hook by replacing `users` array with users `Map`
+
+// * 3. You fixed `OrdersList` by adding bottom margin
